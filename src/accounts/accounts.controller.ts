715dcfeba73fa1230a9693ce8b9815c711cb6586
Bianca Lujan <bblanc545@aol.com>
2021-07-27 11:28:35 -0600

    updated Accounts controller to reflect service

--- src/accounts/accounts.controller.ts
+++ src/accounts/accounts.controller.ts
@@ -1,16 +1,21 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateAccountDto } from './dto/create-account.dto';
+import { AccountsService } from './accounts.service';
+import { Account } from './interfaces/account.interface';
 
 @Controller('accounts')
 export class AccountsController {
+  constructor(private accountsService: AccountsService) {}
+
   @Post()
   create(@Body() createAccountDto: CreateAccountDto) {
-    return 'creates a new account';
+    this.accountsService.create(createAccountDto);
   }
+
   //GET all accounts
   @Get()
-  getAccounts(): any {
-    return [{ id: 0 }];
+  findAll(): Account[] {
+    return this.accountsService.findAll();
   }
 
   //GET account by id
