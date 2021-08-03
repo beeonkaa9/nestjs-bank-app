6f47cdc542ce52e36eacbfd71c1974937c01d2eb
Bianca Lujan <bblanc545@aol.com>
2021-08-03 11:02:58 -0600

    creation of transaction module, service, and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AccountsModule } from './accounts/accounts.module';
+import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
-  imports: [AccountsModule],
+  imports: [AccountsModule, TransactionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
