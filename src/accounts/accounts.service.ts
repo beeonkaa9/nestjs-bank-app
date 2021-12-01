3c4ed04c1768b570688a17e1b9291e55bfe94d6f
Bianca Lujan <bblanc545@aol.com>
2021-12-01 16:09:22 -0700

    fixed accounts so they go into array; major cleanup of accounts service

--- src/accounts/accounts.service.ts
+++ src/accounts/accounts.service.ts
@@ -12,51 +12,39 @@ import {
 } from 'src/transactions/entities/transactions.entity';
 import { SendTransactionDto } from 'src/transactions/dto/send-transaction.dto';
 import { sendMoneyValidation } from './functions/sendMoneyLogic';
-import { AccountsModule } from './accounts.module';
 
 //data storage and retrieval (in memory)
 @Injectable()
 export class AccountsService {
-  private readonly accounts: Account[] = [];
-  private readonly transactions: (Transaction | sendTransaction)[] = [];
+  accounts: Account[] = [];
+  transactions: (Transaction | sendTransaction)[] = [];
 
   //getter to return transaction array to transactions service (for findAllTransactions)
   get transactionsArray(): Transaction[] {
     return this.transactions;
   }
 
-  //creates a new account
   create(createAccountDto: CreateAccountDto) {
     const accountId: string = createAccountDto.id;
-    if (this.accounts.find((accounts) => accounts.id == accountId)) {
+    if (this.accounts.find((accounts) => accounts.id === accountId)) {
       throw new NotAcceptableException(
         'an account already exists for this id; cannot make duplicate accounts',
       );
     }
-    /*
-    const newAccount = { ...createAccountDto };
-    this.accounts.push(newAccount);
-    */
-    function newAccount(accounts, createAccountDto) {
-      return [...accounts, { createAccountDto }];
-    }
-    newAccount(this.accounts, createAccountDto);
+    this.accounts = [...this.accounts, { ...createAccountDto }];
   }
 
-  //returns all accounts
   findAll(): Account[] {
     return this.accounts;
   }
 
-  //returns an account based on accountId
   findOne(accountId: string) {
-    if (!this.accounts.find((accounts) => accounts.id == accountId)) {
+    if (!this.accounts.find((accounts) => accounts.id === accountId)) {
       throw new NotFoundException('an account does not exist for this user id');
     }
-    return this.accounts.find((accounts) => accounts.id == accountId);
+    return this.accounts.find((accounts) => accounts.id === accountId);
   }
 
-  //creates a transaction to add money for a specific account
   addMoney(createTransactionDto: CreateTransactionDto) {
     const accountId: string = createTransactionDto.id;
 
@@ -64,15 +52,13 @@ export class AccountsService {
       throw new NotFoundException('an account does not exist for this user id');
     }
 
-    const newTransaction = { ...createTransactionDto };
-    this.transactions.push(newTransaction);
+    this.transactions = [...this.transactions, { ...createTransactionDto }];
 
     //access account to add money
     const account: Account = this.findOne(accountId);
-    account.balance.amount += newTransaction.amount_money.amount;
+    account.balance.amount += createTransactionDto.amount_money.amount;
   }
 
-  //creates a transaction to withdraw money for a specific account
   withdraw(createTransactionDto: CreateTransactionDto) {
     const accountId: string = createTransactionDto.id;
 
@@ -80,28 +66,25 @@ export class AccountsService {
       throw new NotFoundException('an account does not exist for this user id');
     }
 
-    const newTransaction = { ...createTransactionDto };
-    this.transactions.push(newTransaction);
+    this.transactions = [...this.transactions, { ...createTransactionDto }];
 
     //access account to subtract money
     const account: Account = this.findOne(accountId);
-    if (account.balance.amount < newTransaction.amount_money.amount) {
+    if (account.balance.amount < createTransactionDto.amount_money.amount) {
       throw new NotAcceptableException(
         'not enough money in balance to make this withdrawal',
       );
     }
-    account.balance.amount -= newTransaction.amount_money.amount;
+    account.balance.amount -= createTransactionDto.amount_money.amount;
   }
 
-  //send money to another account
   sendMoney(sendTransactionDto: SendTransactionDto) {
-    //to check if id and target_account_id exist
     const senderId: string = sendTransactionDto.id;
     const targetId: string = sendTransactionDto.target_account_id;
 
     //for the validation checks on the sender account's balance and amount to be sent
     const amountToSend: number = sendTransactionDto.amount_money.amount;
-    const idBalance: number = this.findOne(senderId).balance.amount;
+    const senderBalance: number = this.findOne(senderId).balance.amount;
 
     //to perform the send; withdraw from sender and add to receiver
     const senderTransaction: CreateTransactionDto = {
@@ -122,38 +105,34 @@ export class AccountsService {
       );
     }
 
-    /*does all the checks for the following business rules:
-    +ensure that money sent is between 1 and 1000
-    +ensure that money sent cannot be more than balance
-    +cannot send money if balance is zero
+    /*
+      does all the checks for the following business rules:
+        ensure that money sent is between 1 and 1000
+        ensure that money sent cannot be more than balance
+        cannot send money if balance is zero
     */
-    sendMoneyValidation(amountToSend, idBalance);
+    sendMoneyValidation(amountToSend, senderBalance);
 
     //make sure transaction goes through completely
     try {
       this.withdraw(senderTransaction);
       this.addMoney(targetTransaction);
     } catch (e) {
-      return 'something went wrong' + e;
+      return 'something went wrong' + e.message;
     }
 
-    //push the send transaction to transaction array
-    const newTransaction = { ...sendTransactionDto };
-    this.transactions.push(newTransaction);
+    this.transactions = [...this.transactions, { ...sendTransactionDto }];
   }
 
-  //find all transactions for a specific id
   findAllforId(accountId: string) {
-    const transactionsForId: Transaction[] = [];
     if (!this.findOne(accountId)) {
       throw new NotFoundException('an account does not exist for this user id');
     }
 
-    for (const transaction of this.transactions) {
-      if (transaction.id === accountId) {
-        transactionsForId.push(transaction);
-      }
-    }
-    return transactionsForId;
+    const transactionsOfId = this.transactions.filter(
+      (transaction) => transaction.id === accountId,
+    );
+
+    return transactionsOfId;
   }
 }
